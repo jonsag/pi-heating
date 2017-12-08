bd8ae5f82d770f2c8d57a5ced0d47d8ddcc2d945
jon <jon@HP-Pavilion-15>
2017-12-08 22:00:16 +0100

    Added install for pi-heating-weather

--- README.md
+++ README.md
@@ -11,14 +11,18 @@ or
 $ cd /home/pi
 $ git clone https://github.com/jonsag/pi-heating.git
 
+
 On pi running as hub or hub/remote:
 $ sudo pi-heating/pi-heating-hub-install.sh
 $ sudo pi-heating/pi-heating-hub-mysql-install.s
 $ sudo pi-heating/pi-heating-remote-install.sh
 $ sudo pi-heating/pi-heating-hub-secure.sh
 
+
 If hub also will have extended weather and power logging:
 $ sudo pi-heating/pi-heating-weather-install.sh
+		wget "http://www.triconsole.com/php/calendar_download.php" -O "/home/pi/calendar.zip"
+
 
 On pi running solely as remote:
 $ sudo pi-heating/pi-heating-remote-install.sh
--- pi-heating-weather-install.sh
+++ pi-heating-weather-install.sh
@@ -9,11 +9,13 @@
 #set -e
 clear
 
+
 if [[ `whoami` != "root" ]]
 then
   printf "\n\n Script must be run as root. \n\n"
   exit 1
 fi
+
 
 OS_VERSION=$(cat /etc/os-release | grep VERSION=)
 if [[ $OS_VERSION != *"stretch"* ]]
@@ -21,6 +23,7 @@ then
   printf "\n\n EXITING : Script must be run on PI OS Stretch. \n\n"
   exit 1
 fi
+
 
 APACHE_INSTALLED=$(which apache2)
 if [[ "$APACHE_INSTALLED" == "" ]]
@@ -42,6 +45,7 @@ else
   printf "\n\n Apache is already installed. \n"
 fi
 
+
 PHP_INSTALLED=$(which php)
 if [[ "$PHP_INSTALLED" == "" ]]
 then
@@ -58,6 +62,7 @@ then
 else
   printf "\n\n PHP is already installed. \n"
 fi
+
 
 MYSQL_INSTALLED=$(which mysql)
 if [[ "$MYSQL_INSTALLED" == "" ]]
@@ -76,7 +81,6 @@ else
   printf "\n\n MYSQL is already installed. \n"
 fi
 
-# sudo apt-get install php5-mysql
 
 PHPMYSQL_INSTALLED=$(find /var/lib/dpkg -name php-mysql*)
 if [[ "$PHPMYSQL_INSTALLED" == "" ]]
@@ -95,7 +99,6 @@ else
   printf "\n\n MYSQL PHP Module is already installed. \n"
 fi
 
-# apt-get install python3-mysqldb -y
 
 PYMYSQL_INSTALLED=$(find /var/lib/dpkg -name python-mysql*)
 if [[ "$PYMYSQL_INSTALLED" == "" ]]
@@ -115,115 +118,60 @@ else
 fi
 
 
-RRD_INSTALLED=$(find /var/lib/dpkg -name rrdtool*)
-if [[ "$RRD_INSTALLED" == "" ]]
+if [ ! -f "/home/pi/pi-heating-weather/README.md" ]
 then
-  printf "\n\n Installing RRD tool ...\n"
-  # Install Apache
-  apt-get install rrdtool php-rrd -y
+  printf "\n\n Installing pi-heating-weather ...\n"
 
-  RRD_INSTALLED=$(find /var/lib/dpkg -name rrdtool*)
-    if [[ "$RRD_INSTALLED" == "" ]]
-    then
-      printf "\n\n EXITING : RRD tool installation FAILED\n"
-      exit 1
-    fi
-else
-  printf "\n\n RRD tool is already installed. \n"
-fi
-
-#sudo apt-get install nmap
-
-NMP_INSTALLED=$(find /var/lib/dpkg -name nmap*)
-if [[ "$NMP_INSTALLED" == "" ]]
-then
-  printf "\n\n Installing nmap ...\n"
-  # Install Apache
-  apt-get install nmap -y
-
-  NMP_INSTALLED=$(find /var/lib/dpkg -name nmap*)
-    if [[ "$NMP_INSTALLED" == "" ]]
-    then
-      printf "\n\n EXITING : nmap  installation FAILED\n"
-      exit 1
-    fi
-else
-  printf "\n\n nmap is already installed. \n"
-fi
-
-# Install 'pi-heating-hub' app
-
-PI_HEATING_V='1.0.0'
-if [ ! -f "/home/pi/pi-heating-hub/README.md" ]
-then
-  printf "\n\n Installing pi-heating-hub v$PI_HEATING_V ...\n"
-  # Install Apache
   cd /home/pi
-  if [ -d "/home/pi/pi-heating-hub" ]
-  then
-    rm -rf "/home/pi/pi-heating-hub"
-  fi
-
-  if [ -d "/var/www/pi-heating-hub" ]
-  then
-    rm -rf "/var/www/pi-heating-hub"
-  fi
-
-  wget "https://github.com/JeffreyPowell/pi-heating-hub/archive/$PI_HEATING_V.tar.gz" -O "/home/pi/pi-heating-hub.tar.gz"
-  tar -xvzf "/home/pi/pi-heating-hub.tar.gz"
-  rm "/home/pi/pi-heating-hub.tar.gz"
-  mv "/home/pi/pi-heating-hub-$PI_HEATING_V" "/home/pi/pi-heating-hub"
-  mv "/home/pi/pi-heating-hub/www" "/var/www/pi-heating-hub"
-  chown -R pi:www-data "/home/pi/pi-heating-hub"
-  chmod -R 750 "/home/pi/pi-heating-hub"
   
-  if [ ! -d "/var/www/pi-heating-hub/data" ]
+  if [ -d "/home/pi/pi-heating-weather" ]
   then
-    mkdir "/home/pi/pi-heating-hub/data"
+    rm -rf "/home/pi/pi-heating-weather"
   fi
-  
-  mkdir "/home/pi/pi-heating-hub/data"
-  chown -R pi:www-data "/home/pi/pi-heating-hub/data"
-  chmod -R 775 "/home/pi/pi-heating-hub/data"
-  chown -R pi:www-data "/var/www/pi-heating-hub"
-  chmod -R 755 "/var/www/pi-heating-hub"
-  chmod -R 775 "/var/www/pi-heating-hub/images"
 
-  if [ ! -f "/home/pi/pi-heating-hub/README.md" ]
+  mv "/home/pi/pi-heating/pi-heating-weather" "/home/pi/pi-heating-weather"
+  mv "/home/pi/pi-heating-weather/www" "/var/www/pi-heating-weather"
+  
+  chown -R pi:pi "/home/pi/pi-heating-weather"
+  chmod -R 750 "/home/pi/pi-heating-weather"
+
+  chown -R pi:www-data "/var/www/pi-heating-weather"
+  chmod -R 755 "/var/www/pi-heating-weather"
+
+  if [ ! -f "/home/pi/pi-heating-weather/README.md" ]
     then
-      printf "\n\n EXITING : pi-heating-hub v$PI_HEATING_V installation FAILED\n"
+      printf "\n\n EXITING : pi-heating-weather installation FAILED\n"
       exit 1
     fi
 
 else
-  printf "\n\n pi-heating-hub v$PI_HEATING_V is already installed. \n"
+  printf "\n\n pi-heating-weather is already installed. \n"
 fi
 
-if [ ! -f "/etc/cron.d/pi-heating" ]
+if [ ! -f "/etc/cron.d/pi-weather" ]
   then
-    cat > /etc/cron.d/pi-heating <<CRON
-* * * * * pi /bin/bash /home/pi/pi-heating-hub/cron/wrapper.sh
+    cat > /etc/cron.d/pi-weathher <<CRON
+* * * * * pi /bin/bash /home/pi/pi-heating-weather/cron/wrapper.sh
 CRON
     service cron restart
   fi
 
 
-
 # configure app
 
-# configure apache vh on port 8080
+# configure apache virtual host on port 8082
 
 printf "\n\n Configuring Apache ...\n"
 
-  cat > /etc/apache2/ports.conf <<PORTS
-Listen 8080
+  cat >> /etc/apache2/ports.conf <<PORTS
+Listen 8082
 PORTS
 
-  cat > /etc/apache2/sites-available/pi-heating-hub.conf <<VHOST
-<VirtualHost *:8080>
+  cat > /etc/apache2/sites-available/pi-heating-weather.conf <<VHOST
+<VirtualHost *:8082>
     ServerAdmin webmaster@localhost
-    DocumentRoot /var/www/pi-heating-hub/
-    <Directory /var/www/pi-heating-hub/>
+    DocumentRoot /var/www/pi-heating-weather/
+    <Directory /var/www/pi-heating-weather/>
         Options -Indexes
         AllowOverride all
         Order allow,deny
@@ -235,7 +183,7 @@ PORTS
 </VirtualHost>
 VHOST
 
-a2ensite pi-heating-hub.conf
+a2ensite pi-heating-weather.conf
 service apache2 restart
 
 printf "\n\n Installation Complete. Some changes might require a reboot. \n\n"
