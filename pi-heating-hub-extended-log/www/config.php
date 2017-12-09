083a210eb43b63f41eccea48aa5ef7e9a0226204
jon <jon@HP-Pavilion-15>
2017-12-09 13:52:06 +0100

    Connect to db

--- pi-heating-hub-extended-log/www/config.php
+++ pi-heating-hub-extended-log/www/config.php
@@ -1,54 +1,59 @@
 <?php
 
-date_default_timezone_set('Europe/Stockholm');
-//setlocale(LC_ALL, 'en_US');
-
-// Connections Parameters
-$db_host = "localhost";
-$db_name = "powerTempLog";
-$username = "arduino";
-$password = "arduinopass";
-
-$db_con = mysql_connect($db_host,$username,$password);
-$connection_string = mysql_select_db($db_name);
-
-// Connection
-//mysql_connect($db_host,$username,$password);
-//mysql_select_db($db_name);
-
-$powerUrl = 'http://arduino01';
-$powerPollReset = 'http://arduino01/?pollReset';
-
-$tempUrl = 'http://localhost/jsPowerTempLog/temperature.php';
-$tempPollReset = 'http://localhost/jsPowerTempLog/tempPollReset.php';
-
-$weatherUrl = 'http://localhost/jsPowerTempLog/weather.php';
-$weatherPollReset = 'http://localhost/jsPowerTempLog/weather.php?poll=1';
-
-// power poller
-$powerCurrentTimeMatch = "Current time: ";
-// $lastSyncMatch = "";
-// $skewMatch = "";
-$powerUnixTimeMatch = "Unix time: ";
-// $loopTimeMatch = "";
-$powerCurrentValueMatch = "Current phase ";
-$powerCurrentAverageValueMatch = "Current average phase ";
-$powerTempValueMatch = "Temperature sensor ";
-$powerPulsesLastIntervalMatch = "Pulses last interval: ";
-$powerPulsesMatch = "Pulses since last poll: ";
-
-// temp poller
-$tempTempValueMatch = "Temperature sensor ";
-
-// weather poller
-$weatherPollStartMatch = "pollStart";
-$weatherPollEndMatch = "pollEnd";
-
-$weatherWindDirMatch = "Wind direction: ";
-$weatherWindDirDegMatch = "Wind direction degrees: ";
-$weatherAvgWindDirDegMatch = "Average wind direction degrees: ";
-$weatherWindSpeedMatch = "Wind speed: ";
-$weatherAverageWindSpeedMatch = "Average wind speed: ";
-$weatherRainSinceLastMatch = "Rain since last poll: ";
+    date_default_timezone_set('Europe/Stockholm');
+    //setlocale(LC_ALL, 'en_US');
+    
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+    $ini_array = parse_ini_file("/home/pi/pi-heating-hub/config/config.ini", true);
+    
+    $servername = $ini_array['db']['server'];
+    $username =$ini_array['db']['user'];
+    $password = $ini_array['db']['password'];
+    $dbname = $ini_array['db']['database'];
+    
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("<br><br>Connection failed: " . mysqli_connect_error());
+    }
+    
+    $powerUrl = 'http://arduino01';
+    $powerPollReset = 'http://arduino01/?pollReset';
+    
+    $tempUrl = 'http://localhost/jsPowerTempLog/temperature.php';
+    $tempPollReset = 'http://localhost/jsPowerTempLog/tempPollReset.php';
+    
+    $weatherUrl = 'http://localhost/jsPowerTempLog/weather.php';
+    $weatherPollReset = 'http://localhost/jsPowerTempLog/weather.php?poll=1';
+    
+    // power poller
+    $powerCurrentTimeMatch = "Current time: ";
+    // $lastSyncMatch = "";
+    // $skewMatch = "";
+    $powerUnixTimeMatch = "Unix time: ";
+    // $loopTimeMatch = "";
+    $powerCurrentValueMatch = "Current phase ";
+    $powerCurrentAverageValueMatch = "Current average phase ";
+    $powerTempValueMatch = "Temperature sensor ";
+    $powerPulsesLastIntervalMatch = "Pulses last interval: ";
+    $powerPulsesMatch = "Pulses since last poll: ";
+    
+    // temp poller
+    $tempTempValueMatch = "Temperature sensor ";
+    
+    // weather poller
+    $weatherPollStartMatch = "pollStart";
+    $weatherPollEndMatch = "pollEnd";
+    
+    $weatherWindDirMatch = "Wind direction: ";
+    $weatherWindDirDegMatch = "Wind direction degrees: ";
+    $weatherAvgWindDirDegMatch = "Average wind direction degrees: ";
+    $weatherWindSpeedMatch = "Wind speed: ";
+    $weatherAverageWindSpeedMatch = "Average wind speed: ";
+    $weatherRainSinceLastMatch = "Rain since last poll: ";
 
 ?>
