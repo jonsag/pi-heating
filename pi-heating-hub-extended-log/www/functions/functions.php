69396de0b98a2e8e4bb0972ec2d209c6dd1110c5
jon <jon@HP-Pavilion-15>
2017-12-13 23:10:12 +0100

    Changing some output

--- pi-heating-hub-extended-log/www/functions/functions.php
+++ pi-heating-hub-extended-log/www/functions/functions.php
@@ -35,20 +35,71 @@ function is_cli() {
   }
 }
 
+//function secondsToTime($seconds) {
+//    $dtF = new \DateTime('@0');
+//    $dtT = new \DateTime("@$seconds");
+//    return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
+//}
+
+function secondsToTime($inputSeconds) {
+    $secondsInAMinute = 60;
+    $secondsInAnHour = 60 * $secondsInAMinute;
+    $secondsInADay = 24 * $secondsInAnHour;
+    
+    // Extract days
+    $days = floor($inputSeconds / $secondsInADay);
+    
+    // Extract hours
+    $hourSeconds = $inputSeconds % $secondsInADay;
+    $hours = floor($hourSeconds / $secondsInAnHour);
+    
+    // Extract minutes
+    $minuteSeconds = $hourSeconds % $secondsInAnHour;
+    $minutes = floor($minuteSeconds / $secondsInAMinute);
+    
+    // Extract the remaining seconds
+    $remainingSeconds = $minuteSeconds % $secondsInAMinute;
+    $seconds = ceil($remainingSeconds);
+    
+    // Format and return
+    $timeParts = [];
+    $sections = [
+        'day' => (int)$days,
+        'hour' => (int)$hours,
+        'minute' => (int)$minutes,
+        'second' => (int)$seconds,
+    ];
+    
+    foreach ($sections as $name => $value){
+        if ($value > 0){
+            $timeParts[] = $value. ' '.$name.($value == 1 ? '' : 's');
+        }
+    }
+    
+    return implode(', ', $timeParts);
+}
+
 function table3columns($column1texts, $column2texts, $column3texts) {
-    $cellpadding = 0;
-    $cellspacing = 10;
+    //$cellpadding = 0;
+    //$cellspacing = 10;
     $columns = 3;
+    
+    // 4 values: top right bottom left
+    // 3 values: top right/left bottom
+    // 2 values: top/bottom right/left
+    // 1 value: top/bottom/right/left
+    $style = "padding: 5px 15px;";
+    
     $rows = sizeof($column1texts);    
     
-    echo "<table cellpadding=" . $cellpadding . " cellspacing=" . $cellspacing . ">\n";
+    //echo "<table cellpadding=" . $cellpadding . " cellspacing=" . $cellspacing . ">\n";
+    echo "<table style='" . $style . "'>\n";
     for ($row = 0; $row <= ($rows - 1); $row++) {
         echo "<tr>" . "\n";
         
         $text =  [ $column1texts[$row],
             $column2texts[$row],
-            $column3texts[$row]
-        ];
+            $column3texts[$row] ];
         
         for ($column = 0; $column <= ($columns - 1); $column++) {
             echo "<th>" . $text[$column] . "</th>" . "\n";
--- pi-heating-hub-extended-log/www/temperature.php
+++ pi-heating-hub-extended-log/www/temperature.php
@@ -14,13 +14,7 @@
 <?php
     include ('config.php');
     include ('functions/functions.php');
-    
-    //var_dump($dallasTemps);
-    
-    $numberOfTemps = 0;
-    $dallasId = "";
-    $temperature = 0;
-    
+        
     $sql = "SELECT * FROM sensors";
     
     $result = $conn->query($sql);
@@ -41,14 +35,18 @@
             $column3texts = [ "" ];
             
             array_push($column1texts, "");
+            array_push($column2texts, "----------");
+            array_push($column3texts, "");
+            
+            array_push($column1texts, "");
             array_push($column2texts, "");
             array_push($column3texts, "");
             
-            array_push($column1texts, "id");
+            array_push($column1texts, "Sensor id");
             array_push($column2texts, $row["id"]);
             array_push($column3texts, "");
             
-            array_push($column1texts, "ip");
+            array_push($column1texts, "Host");
             array_push($column2texts, $row["ip"]);
             array_push($column3texts, "");
             
@@ -56,16 +54,59 @@
             array_push($column2texts, $row["ref"]);
             array_push($column3texts, "");
             
-            array_push($column1texts, "");
-            array_push($column2texts, "");
+            $sql3 = "SELECT COUNT(*) AS count FROM tempLog WHERE 
+sensorid='" . $row["id"] . "'";
+            $result3 = $conn->query($sql3);
+            if ($result3->num_rows > 0) {
+                while($row3 = $result3->fetch_assoc()) {
+                    $noofvalues = $row3["count"];
+                    array_push($column1texts, "No of values");
+                    array_push($column2texts, $noofvalues);
+                    array_push($column3texts, "");
+                }
+            }
+            
+            $sql4 = "SELECT 
+(SELECT ts FROM tempLog WHERE 
+sensorid='" . $row["id"] . "' ORDER BY ts LIMIT 1) as 'first', 
+(SELECT ts FROM tempLog WHERE 
+sensorid='" . $row["id"] . "' ORDER BY ts DESC LIMIT 1) as 'last'";
+            $result4 = $conn->query($sql4);
+            if ($result4->num_rows > 0) {
+                while($row4 = $result4->fetch_assoc()) {
+                    $starttime = $row4["first"];
+                    $stoptime = $row4["last"];
+                    array_push($column1texts, "First / last");
+                    array_push($column2texts, $starttime);
+                    array_push($column3texts, $stoptime);
+                }
+            }
+            
+            $difftime = abs(strtotime($stoptime) - strtotime($starttime));
+            $diff = $difftime / 60;
+            array_push($column1texts, "Diff");
+            array_push($column2texts, round($diff) . " minutes");
             array_push($column3texts, "");
             
             array_push($column1texts, "");
-            array_push($column2texts, "Date Time");
+            array_push($column2texts, secondsToTime(round($difftime)));
+            array_push($column3texts, "");
+            
+            $valuesperminute = round($noofvalues / $diff, 2);
+            array_push($column1texts, "Avg values/min");
+            array_push($column2texts, $valuesperminute);
+            array_push($column3texts, "");
+            
+            array_push($column1texts, "");
+            array_push($column2texts, "----------");
+            array_push($column3texts, "");
+            
+            array_push($column1texts, "");
+            array_push($column2texts, "Date, time");
             array_push($column3texts, "deg C");
             
             array_push($column1texts, "");
-            array_push($column2texts, date('D j/n -y G:i', time()));
+            array_push($column2texts, date('D j/n -y, G:i', time()));
             array_push($column3texts, $row["value"]);
             
             $sql2 = "SELECT * FROM tempLog WHERE 
@@ -77,7 +118,7 @@ sensorid='" . $row["id"]. "') LIMIT 1";
             if ($result2->num_rows > 0) {
                 while($row2 = $result2->fetch_assoc()) {
                     array_push($column1texts, "Lowest");
-                    array_push($column2texts, date('D j/n -y G:i', strtotime($row2["ts"])));
+                    array_push($column2texts, date('D j/n -y, G:i', strtotime($row2["ts"])));
                     array_push($column3texts, $row2["value"]);
                 }
             }
@@ -91,7 +132,7 @@ sensorid='" . $row["id"]. "') LIMIT 1";
             if ($result2->num_rows > 0) {
                 while($row2 = $result2->fetch_assoc()) {
                     array_push($column1texts, "Highest");
-                    array_push($column2texts, date('D j/n -y G:i', strtotime($row2["ts"])));
+                    array_push($column2texts, date('D j/n -y, G:i', strtotime($row2["ts"])));
                     array_push($column3texts, $row2["value"]);
                 }
             }
