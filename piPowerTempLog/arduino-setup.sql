a90db47739746ae11fd35a681360a7b1ce26c82b
jon <jonsagebrand@gmail.com>
2020-02-23 17:06:44 +0100

    cleaned up

--- piPowerTempLog/arduino-setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE if not exists arduino1;
-GRANT USAGE ON *.* to arduino@localhost IDENTIFIED BY 'arduinopass';
-GRANT ALL PRIVILEGES ON arduino1.* TO arduino@localhost WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-ALTER DATABASE arduino1 DEFAULT CHARACTER SET latin1;
